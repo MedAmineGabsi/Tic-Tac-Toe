413aa864d230c9f3d6201fd61c429376c7ce08ef
Amine GABSI <mamine.gabsi@gmail.com>
2022-01-31 01:02:48 +0100

    adding dashboard

--- src/App.js
+++ src/App.js
@@ -1,13 +1,102 @@
-import React from 'react';
-import Board from './components/Board'
+import React from "react";
+import { useState } from "react/cjs/react.development";
+import Board from "./components/Board";
 
 const App = () => {
+  const [inGame, setInGame] = useState(false);
+
+  const [playerOne, setPlayerOne] = useState("");
+  const [playerTwo, setPlayerTwo] = useState("");
+
+  const [playerOneError, setPlayerOneError] = useState(" ");
+  const [playerTwoError, setPlayerTwoError] = useState(" ");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const user = { playerOne, playerTwo };
+    console.log(user);
+    setInGame(true);
+  };
+
+  const handlePlayerOne = (e) => {
+    setPlayerOne(e.target.value);
+    if (e.target.value.length < 1) {
+      setPlayerOneError(" ");
+    } else if (e.target.value.length < 3) {
+      setPlayerOneError("The nickname must be at least 3 characters or more");
+    } else {
+      setPlayerOneError("");
+    }
+  };
+
+  const handlePlayerTwo = (e) => {
+    setPlayerTwo(e.target.value);
+    if (e.target.value.length < 1) {
+      setPlayerTwoError(" ");
+    } else if (e.target.value.length >= 3) {
+      setPlayerTwoError("");
+    } else if (e.target.value.length > 0 || e.target.value.length < 3) {
+      setPlayerTwoError("The nickname must be at least 3 characters or more");
+    }
+  };
   return (
     <div className="App">
-      <h1 className='header'>TIC TAC TOE</h1>
-      <Board/>
+      <h1 className="header" onClick={() => setInGame(false)}>
+        TIC TAC TOE
+      </h1>
+      {inGame ? (
+        <Board playerOne={playerOne} playerTwo={playerTwo} />
+      ) : (
+        <div className="menu">
+          <>
+            <form style={{ margin: "32px" }} onSubmit={handleSubmit}>
+              <>
+                <div className="input-container">
+                  <input
+                    placeholder="Player 1"
+                    value={playerOne}
+                    onChange={handlePlayerOne}
+                    name="Player1"
+                    required
+                  />
+                  <br />
+                  {playerOneError ? (
+                    <label className="error">{playerOneError}</label>
+                  ) : (
+                    ""
+                  )}
+                </div>
+                <input
+                  placeholder="Player 2"
+                  value={playerTwo}
+                  onChange={handlePlayerTwo}
+                  name="player 2"
+                  required
+                />
+                <br />
+                {playerTwoError ? (
+                  <label className="error">{playerTwoError}</label>
+                ) : (
+                  ""
+                )}
+                <br />
+                {!playerOneError && !playerTwoError ? (
+                  <button type="submit" className="enabled">
+                    Submit
+                  </button>
+                ) : (
+                  <button type="submit" className="disabled" disabled={true}>
+                    Submit
+                  </button>
+                )}
+              </>
+            </form>
+          </>
+        </div>
+      )}
     </div>
   );
-}
+};
 
 export default App;
--- src/components/Board.js
+++ src/components/Board.js
@@ -1,10 +1,49 @@
 import React, { useState } from "react";
 import Square from "./Square";
+import swal from "sweetalert";
 
-const Board = () => {
+const Board = (props) => {
   const [square, setSquare] = useState(Array(9).fill(null));
   const [X, setX] = useState(true);
 
+  const calculateWinner = (squares) => {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+
+      if (
+        squares[a] &&
+        squares[a] === squares[b] &&
+        squares[a] === squares[c]
+      ) {
+        return squares[a];
+      }
+    }
+    return null;
+  };
+  var winner = calculateWinner(square);
+  let status;
+  if (winner) {
+    status =
+      "The Winner is " + (winner === "X" ? props.playerOne : props.playerTwo);
+  } else {
+    status = "Turn of: " + (X ? props.playerOne : props.playerTwo);
+  }
+
+  const reset = () =>{
+    setSquare(Array(9).fill(null))
+    winner = null
+  }
   const renderSquare = (position) => {
     return (
       <Square value={square[position]} onClick={() => handleClick(position)} />
@@ -13,13 +52,24 @@ const Board = () => {
 
   const handleClick = (position) => {
     const squares = square.slice();
-
-    squares[position] = X ? "X" : "O";
-    setSquare(squares);
-    setX(!X);
+    if (squares[position] === null) {
+      squares[position] = X ? "X" : "O";
+      setSquare(squares);
+      setX(!X);
+    } else {
+      swal(
+        "Oops 😶",
+        "This case is already taken by the player: " +
+          (squares[position] === "X" ? props.playerOne : props.playerTwo),
+        "error"
+      );
+    }
   };
+
   return (
     <div className="board">
+      <div className="board-row">{props.playerOne} = X</div>
+      <div className="board-row">{props.playerTwo} = O</div>
       <div className="board-row">
         {renderSquare(0)}
         {renderSquare(1)}
@@ -35,6 +85,20 @@ const Board = () => {
         {renderSquare(7)}
         {renderSquare(8)}
       </div>
+
+      <h3>{status}</h3>
+      <button
+        style={{
+          border: "none",
+          padding: "10px",
+          backgroundColor: "#caccc8",
+          color: "red",
+          borderRadius: "5px",
+        }}
+        onClick={() => reset()}
+      >
+        Restart
+      </button>
     </div>
   );
 };
